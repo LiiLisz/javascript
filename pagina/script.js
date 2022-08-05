a42cd57f2f82c0ad980c01b330b27cad58aa94cd
Liara Bessa <liara_lia@hotmail.com>
2022-08-05 16:54:23 -0300

    Atualizado com success

--- pagina/script.js
+++ pagina/script.js
@@ -1,55 +1,53 @@
-const form = document.getElementById('form');
-const nome = document.getElementById('nome');
-const email = document.getElementById('email');
-const senha = document.getElementById('senha');
-const confirmacao = document.getElementById('confirmacao');
+const form = document.getElementById('form')
+const nome = document.getElementById('nome')
+const email = document.getElementById('email')
+const senha = document.getElementById('senha')
+const confirmacao = document.getElementById('confirmacao')
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-    checkInputs();
+    checkInputs()
 });
 
 function checkInputs() {
-    const nomeValue = nome.value;
-    const emailValue = email.value;
-    const senhaValue = senha.value;
-    const confirmacaoValue = confirmacao.value;
+    const nomeValue = nome.value.trim()
+    const emailValue = email.value.trim()
+    const senhaValue = senha.value.trim()
+    const confirmacaoValue = confirmacao.value.trim()
 
-    if (nomeValue === "") {
-        setErrorFor(nome, "O nome de usuário é obrigatório.");
+    if (nomeValue === '') {
+        setErrorFor(nome, 'O nome de usuário é obrigatório.')
     } else {
-        setSuccessFor(nome);
+        setSuccessFor(nome)
     }
 
-    if (emailValue === "") {
-        setErrorFor(email, "O email é obrigatório.");
-    } else if (!checkEmail(emailValue)) {
-        setErrorFor(email, "Por favor insira um email válido.");
+    if (emailValue === '') {
+        setErrorFor(email, 'O email é obrigatório.')
     } else {
-        setSuccessFor(email);
+        setSuccessFor(email)
     }
 
-    if (senhaValue === "") {
-        setErrorFor(senha, "A senha é obrigatória.");
+    if (senhaValue === '') {
+        setErrorFor(senha, 'A senha é obrigatória.')
     } else if (senhaValue.length < 8) {
-        setSuccessFor(senha, "A senha tem que ter no mínimo 8 caracteres.");
+        setErrorFor(senha, 'A senha tem que ter no mínimo 8 caracteres.')
     } else {
-        setSuccessFor(senha);
+        setSuccessFor(senha)
     }
     
-    if (confirmacaoValue === "") {
-        setErrorFor(confirmacao, "A confirmação da senha é obrigatória.");
-    } else if (confirmacaoValue != senhaValue) {
-        setErrorFor(confirmacao, "A senhas não conferem.");
+    if (confirmacaoValue === '') {
+        setErrorFor(confirmacao, 'A confirmação da senha é obrigatória.')
+    } else if (confirmacaoValue !== senhaValue) {
+        setErrorFor(confirmacao, 'A senhas não conferem.')
     } else {
-        setSuccessFor(confirmacao);
+        setSuccessFor(confirmacao)
     }
 
-    const formControls = form.querySelectorAll(".form-control");
+    const formControls = form.querySelectorAll(".form-control")
     
     const formIsValid = [...formControls].every((formControl) => {
-        return (formControl.className === "form-control success");
+        return (formControl.className === "form-control success")
     });
 
     if (formIsValid) {
@@ -59,19 +57,16 @@ function checkInputs() {
 
 function setErrorFor(input, message) {
     const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
+    const small = formControl.querySelector('small');
 
-    small.innerText = message;
+    small.innerText = message
 
-    formControl.className = "form-control error";
+    formControl.className = "form-control error"
 }
 
 function setSuccessFor(input) {
     const formControl = input.parentElement;
     
-    formControl.className = "form-control success";
+    formControl.className = "form-control success"
 }
 
-function checkEmail(email) {
-   return / ^ (([ ^<>()\[\]\\ .,;: \s @"] + (\.[ ^<>()\[ \] \\ .,;: \s @"]+ )*) | ( ". + " )) @ ((\[ [ 0-9 ] { 1,3 } \. [ 0-9 ] { 1,3 } \. [ 0-9 ]{ 1,3 } \. [ 0-9 ] { 1,3 } ] ) | ( ( [ a-zA-Z \- 0-9 ] + \. ) + [ a-zA-Z ] { 2, } ) ) $ / .teste(email);
-}
