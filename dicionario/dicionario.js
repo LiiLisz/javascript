7c6e948893d76e49f4dfd93572b677e05e11a8c6
LiaraSampaio <liara_lia@hotmail.com>
2022-09-08 13:42:22 -0300

    Fim

--- dicionario/dicionario.js
+++ dicionario/dicionario.js
@@ -1,35 +1,23 @@
-let formulario = document.querySelector(".js-formulario");
-let resultado = document.querySelector(".js-resultado");
-let carregamento = document.querySelector(".js-carregamento");
-let resultadoTitulo = document.querySelector(".js-resultado_titulo");
-let resultadoDescricao = document.querySelector(".js-resultado_descricao");
+let url = 'https://significado.herokuapp.com/v2/';
 
-formulario.addEventListener("submit", (evento) => {
-    evento.preventDefault();
+let inputTxt = document.querySelector('#container__inputtxt');
+let btnTxt = document.querySelector('#container__btn');
+let resultado = document.querySelector('#container__resultado');
 
-    resultado.classList.remove("display-none");
-    carregamento.classList.remove("display-none");
-
-    let palavra = evento.target[0].value;
-    let url = `https://api.dicionario-aberto.net/word/${palavra}`;
-
-    fetch(url)
+btnTxt.addEventListener('click', () => {
+    let palavra = inputTxt.value;
+    if(palavra === ''){
+        resultado.innerHTML = `<p id="container__significado"><span>Escreva uma palavra no campo de busca!</span></p>`
+    } else {
+        fetch(`${url}${palavra}`)
         .then((resposta) => resposta.json())
-        .then((resposta) => {
-            if(!resposta[0]) {
-                resultadoTitulo.textContent = "Palavra não encontrada, verifique a grafia e tente novamente!";
-                resultadoDescricao.textContent = "";
-                return;
-            } 
-
-            let funcaoDeParseamento, elementoParseado;
-            funcaoDeParseamento = new DOWParser();
-            elementoParseado = funcaoDeParseamento.parseFromString(resposta[0].xml, "text/xml");
-            
-            resultadoTitulo.textContent = elementoParseado.getElementByTagName("form")[0].getElementByTagName("orth")[0].textContent;
-            resultadoDescricao.textContent = elementoParseado.getElementByTagName("sense")[0].getElementByTagName("def")[0].textContent;
-        })
-        .finally(() => {
-            carregamento.classList.add("display-none");
-        });
-});
+        .then((data) => { console.log(data) 
+            resultado.innerHTML = `<h3 id="container__palavra">${palavra}</h3>
+            <p id="container__significado"><span>1°</span>${data[0].meanings[0]}</p>
+            <p id="container__significado"><span>2°</span>${data[0].meanings[1]}</p>
+            <p id="container__significado"><span>3°</span>${data[0].meanings[2]}</p>`
+            }).catch(() => {
+                resultado.innerHTML = `<p id="container__significado"><span>Não foi possível encontrar essa palavra!</span></p>`
+            })
+        }
+    })
